0ea2e8ec7744292e6b7f2a4526e37b6ea1d99235
Arafullah Shihab <shihabaraf@gmail.com>
2019-04-20 07:57:43 -0400

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { createRootNavigator, getSignIn } from "./router";
+import { isSignedIn } from "./auth";
+import Container from "./router";
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+      checkedSignIn: false
+    };
+  }
+
+  componentDidMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(err => alert("An error occurred"));
+  }
+
+  render() {
+    const { checkedSignIn, signedIn } = this.state;
+    // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+    if (!checkedSignIn) {
+      return null;
+    }
+    getSignIn(signedIn);
+
+    return <Container 
+    />;
+  }
+}
